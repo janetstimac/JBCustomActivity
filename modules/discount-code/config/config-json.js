cdcf392652c09db226d54b42d99d03418bfd06c9
janetstimac <90408261+janetstimac@users.noreply.github.com>
2023-02-09 14:22:13 -0500

    Update config-json.js

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -3,7 +3,7 @@ module.exports = function configJSON(req) {
     workflowApiVersion: '1.1',
     metaData: {
       // the location of our icon file
-      icon: `images/icon.svg`,
+      icon: `https://image.s4.sfmc-content.com/lib/fe9115747361007577/m/1/426356d0-4996-4ae1-917a-329692240cc4.jpg`,
       category: 'customer'
     },
     // For Custom Activity this must say, "REST"
@@ -26,7 +26,7 @@ module.exports = function configJSON(req) {
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
         
-        url: `https://${jbcustomactivity.herokuapp.com}/modules/discount-code/execute`,
+        url: `https://jbcustomactivity.herokuapp.com/modules/discount-code/index.html`,
         // The amount of time we want Journey Builder to wait before cancel the request. Default is 60000, Minimal is 1000
         timeout: 10000,
         // how many retrys if the request failed with 5xx error or network error. default is 0
@@ -39,13 +39,13 @@ module.exports = function configJSON(req) {
     },
     configurationArguments: {
       publish: {
-        url: `https://${jbcustomactivity.herokuapp.com}/modules/discount-code/publish`
+        url: `https://jbcustomactivity.herokuapp.com/publish`
       },
       validate: {
-        url: `https://${jbcustomactivity.herokuapp.com}/modules/discount-code/validate`
+        url: `https://jbcustomactivity.herokuapp.com/validate`
       },
       stop: {
-        url: `https://${jbcustomactivity.herokuapp.com}/modules/discount-code/stop`
+        url: `https://jbcustomactivity.herokuapp.com/stop`
       }
     },
     userInterfaces: {
